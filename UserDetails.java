09f7a4fadefdb57cee345852be6bb8d0c8168fb0
uncle-sweetshare <134929147+uncle-sweetshare@users.noreply.github.com>
2023-06-09 15:32:33 +1200

    new UserDetails

--- UserDetails.java
+++ UserDetails.java
@@ -16,7 +16,7 @@ public class UserDetails {
         this.storeUsers = new HashMap<>();
     }
 
-    //M 8/6: creates a user table. WORKING!
+    //M 9/6: creates a user table. WORKING! added history field
     public void createUserTable(Connection connection) {
         try (Statement statement = connection.createStatement()) {
             connection.setAutoCommit(true); //Start connection
@@ -29,11 +29,12 @@ public class UserDetails {
                 statement.executeUpdate("CREATE TABLE users ("
                         + "id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, "
                         + "username VARCHAR(255) UNIQUE NOT NULL, "
-                        + "password VARCHAR(255) NOT NULL)"
+                        + "password VARCHAR(255) NOT NULL, "
+                        + "history VARCHAR(255))"
                 );
 
                 //Dummy users for testing purposes
-                statement.executeUpdate("INSERT INTO users (username, password) VALUES ('user1', 'pass1')");
+                statement.executeUpdate("INSERT INTO users (username, password, history) VALUES ('user1', 'pass1', 'Cats')");
                 statement.executeUpdate("INSERT INTO users (username, password) VALUES ('user2', 'pass2')");
 
                 System.out.println("Users table created successfully.");
@@ -46,7 +47,7 @@ public class UserDetails {
         }
     }
     
-    //yeah i think this works
+    //M 9/8: updated slightly. still users table not found in GUI???
     public boolean login(Connection connection, String username, String password) {
         boolean matching = false;
 
@@ -71,24 +72,26 @@ public class UserDetails {
         return matching;
     }
 
-    //think this works ok
+    //M 9/8: updated slightly, hopefully should be working now???
     public boolean register(Connection connection, String username, String password) {
         boolean match = false;
 
         try (PreparedStatement statement = connection.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)")) {
             statement.setString(1, username);
             statement.setString(2, password);
-            statement.executeUpdate();
-            storeUsers.put(username, new User(username, password));
-            match = true;
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                storeUsers.put(username, new User(username, password));
+                match = true;
+            }
         } catch (SQLException e) {
-            System.out.println("\nThis user already exists. Login or register a new user.");
+            System.out.println("\nError! This user already exists. Login or register a new user." + e.getMessage());
         }
 
         return match;
     }
 
-    //think this works - it should retrieve info from the DB to populate the storeUsers hashmap. just make sure the map is instantiated before calling it
+    //M 9/8: think this works - it should retrieve info from the DB to populate the storeUsers hashmap. just make sure the map is instantiated before calling it
     public void retrieveUsers(Connection connection) {
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
@@ -99,36 +102,47 @@ public class UserDetails {
                 storeUsers.put(username, new User(username, password));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Error occurred while retrieving users: " + e.getMessage());
         }
     }
 
-    //meant to getShowsBooked from the userobj
-    public void printHistory(Connection connection, String username) throws SQLException {
-        User user = storeUsers.get(username);
-        if (user == null) {
-            System.out.println("User not found.");
-            return;
-        }
+    //M 9/8: updated, should return a string of the user's history. it doesn't really print the history any more but it's still called printHistory
+    //gets user's history from database, uses stringbuilder to create a string to return - past shows on different lines. if no shows found, print an error
+    public String printHistory(Connection connection, String username) throws SQLException {
+        StringBuilder historyBuilder = new StringBuilder();
 
-        ArrayList<Show> showsBooked = user.getShowsBooked(); //is it an arraylist? it's an arraylist right?
-        for (Show show : showsBooked) {
-            System.out.println(show.getName());
-        }
-    }
-
-    //ummmm
-    public void saveHistory(Connection connection, String username, String showName) {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO bookings (username, showName) VALUES (?, ?)")) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT history FROM users WHERE username = ?")) {
             statement.setString(1, username);
-            statement.setString(2, showName);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String history = resultSet.getString("history");
+                if (history != null && !history.isEmpty()) {
+                    historyBuilder.append(history);
+                    historyBuilder.append("\n");
+                } else {
+                    historyBuilder.append("No history found for user ").append(username);
+                }
+            } else {
+                historyBuilder.append("User not found: ").append(username);
+            }
+        }
+
+        return historyBuilder.toString();
+    }
+
+    //M 9/8: append showName to user's history in user table
+    public void saveHistory(Connection connection, String username, String showName) {
+        try (PreparedStatement statement = connection.prepareStatement("UPDATE users SET history = CONCAT(IFNULL(history, ''), ?) WHERE username = ?")) { //Concat to add
+            statement.setString(1, showName + "\n");
+            statement.setString(2, username);
             statement.executeUpdate();
 
             User user = storeUsers.get(username);
             if (user != null) {
-                Show addShow = new Show(); //M: "fixed" these couple of lines.
+                Show addShow = new Show();
                 addShow.setName(showName);
-                user.bookShow(addShow); 
+                user.bookShow(addShow);
             }
         } catch (SQLException e) {
             e.printStackTrace();
